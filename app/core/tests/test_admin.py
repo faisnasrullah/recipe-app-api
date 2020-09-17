b87ed6c9051d9441e5fb4e2e1f312f21ebf63196
Fais Nasrullah <fais@automatalabs.co>
2020-09-17 15:37:53 +0700

    Fixed typo and add register our models

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,15 +7,15 @@ class AdminSiteTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.admin_user = get_user_model.Object.create_superuser(
-            email = 'admin@ampasdev.id',
-            password = '12345678'
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@ampasdev.id',
+            password='12345678'
         )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model.Object.create_user(
-            email = 'user@ampasdev.id',
-            password = '12345678',
-            name = 'User 01'
+        self.user = get_user_model().objects.create_user(
+            email='user@ampasdev.id',
+            password='12345678',
+            name='User 01'
         )
 
     def test_users_listed(self):
